3b96604ee4b461886cc69dac2be5c2e717a9e01c
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2025-12-09 20:33:15 +0900

    feat: 🎉 swift package init --type executable

---
+++ Sources/swift-sakura-internet-app-run-playground/swift_sakura_internet_app_run_playground.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct swift_sakura_internet_app_run_playground {
+    static func main() {
+        print("Hello, world!")
+    }
+}
